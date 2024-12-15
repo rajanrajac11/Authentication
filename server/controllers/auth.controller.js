0d17e41f476b3d2feee0ea899c0f5735c4a97229
Rajan <acharya.rajan.raj11@gmail.com>
2024-12-15 22:48:08 +0545

    bcrypt for hashing

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,8 +1,15 @@
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 const signup = async (req, res) => {
   try {
-    const newUser = await User.create(req.body);
+    const { username, email, password } = req.body;
+    const hashedPassword = bcryptjs.hashSync(password, 13);
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
     res.status(200).json("User created successfully");
   } catch (error) {
     res.status(500).json({ message: error.message });
