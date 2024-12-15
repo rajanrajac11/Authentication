8830a835640dcc3842f290c907caa251a20f8c24
Rajan <acharya.rajan.raj11@gmail.com>
2024-12-15 16:27:09 +0545

    routes and controller in index

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
 const app = express();
 dotenv.config();
+app.use(express.json());
+
+app.use("/api/user/", userRouter);
+app.use("/api/auth/", authRouter);
 
 app.get("/hello", (req, res) => {
   res.send("Hello world");
