a369b7c80ba44e0c967e27270d4d1de213e34982
Rajan <acharya.rajan.raj11@gmail.com>
2024-12-16 22:25:23 +0545

    middleware first commit

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
-const signup = async (req, res) => {
+const signup = async (req, res, next) => {
   try {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 13);
@@ -12,7 +12,7 @@ const signup = async (req, res) => {
     });
     res.status(200).json("User created successfully");
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
 export default signup;
--- server/index.js
+++ server/index.js
@@ -26,3 +26,14 @@ mongoose
   .catch((error) => {
     console.log("Connection failed.");
   });
+
+//middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error.";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
