2d5231e86c6527d316e32229f9b03a6868c66c4b
Rajan <acharya.rajan.raj11@gmail.com>
2024-12-18 23:26:29 +0545

    index file for components

---
+++ Frontend/src/Components/Index.js
@@ -0,0 +1,7 @@
+import Header from "./Header";
+import Input from "./Input";
+import Logo from "./Logo";
+import Login from "./Login";
+import Signup from "./Signup";
+
+export { Header, Input, Logo, Login, Signup };
