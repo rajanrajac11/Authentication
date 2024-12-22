b587109e37251ee5942f6fe9b7a50b8a7bedd4da
Rajan <acharya.rajan.raj11@gmail.com>
2024-12-22 23:10:34 +0545

    login api

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
 
 const signup = async (req, res, next) => {
   try {
@@ -11,6 +13,24 @@ const signup = async (req, res, next) => {
       password: hashedPassword,
     });
     res.status(200).json("User created successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Invalid credentials"));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(validUser);
   } catch (error) {
     next(error);
   }
