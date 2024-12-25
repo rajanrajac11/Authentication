a7b6d8ab695b85beb070b6768232dacb1d907309
Rajan <acharya.rajan.raj11@gmail.com>
2024-12-25 12:56:02 +0545

    proper import for export const

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,6 +1,5 @@
 import express from "express";
-import signup, { login } from "../controllers/auth.controller.js";
-
+import { login, signup } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.post("/signup", signup);
