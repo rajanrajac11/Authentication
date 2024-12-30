0e2ea974b4d4b7605dfcc6d170b055552678b145
Rajan <acharya.rajan.raj11@gmail.com>
2024-12-30 22:50:49 +0545

    firebase frontend part

--- Frontend/src/Components/OAuth.jsx
+++ Frontend/src/Components/OAuth.jsx
@@ -1,10 +1,32 @@
 import React from "react";
-import { GoogleAuthProvider } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
+import { app } from "../firebase";
+import { useDispatch } from "react-redux";
+import { loginSuccess } from "../store/userSlice";
 
 function OAuth() {
+  const dispatch = useDispatch();
   const handleGoogleClick = async () => {
     try {
       const provider = new GoogleAuthProvider();
+
+      const auth = getAuth(app);
+
+      const result = await signInWithPopup(auth, provider);
+
+      const res = await fetch("api/auth/google", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
+      const data = await res.json();
+      dispatch(loginSuccess(data));
     } catch (error) {
       console.log("Couldn't login with google.");
     }
