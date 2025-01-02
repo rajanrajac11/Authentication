d7ef7407a8c863531cd266e87591e3d0d918afff
Rajan <acharya.rajan.raj11@gmail.com>
2025-01-02 22:57:48 +0545

    error fix in google login (validUser = user)

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -44,7 +44,7 @@ export const google = async (req, res, next) => {
     const user = await User.findOne({ email: req.body.email });
     if (user) {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-      const { password: hashedPassword, ...rest } = validUser._doc;
+      const { password: hashedPassword, ...rest } = user._doc;
       const expiryDate = new Date(Date.now() + 3600000);
       res
         .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
