3b0557b766187d58136898d5217c03286944a424
Rajan <acharya.rajan.raj11@gmail.com>
2025-01-05 23:00:43 +0545

    handling the update form for Profile

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,4 +1,18 @@
+import { errorHandler } from "../utils/error.js";
+
 const test = (req, res) => {
   res.status(200).json("This is working");
 };
 export default test;
+
+//update user
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(401, "You can only update your account"));
+  }
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
--- server/index.js
+++ server/index.js
@@ -3,10 +3,12 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/user/", userRouter);
 app.use("/api/auth/", authRouter);
