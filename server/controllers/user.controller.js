b18dc969fe009607531c8cef6f1e51bf687866b5
Rajan <acharya.rajan.raj11@gmail.com>
2025-01-06 22:53:04 +0545

    update User Controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import { errorHandler } from "../utils/error.js";
-
+import bcryptjs from "bcryptjs";
+import User from "../models/user.model.js";
 const test = (req, res) => {
   res.status(200).json("This is working");
 };
@@ -12,6 +13,22 @@ export const updateUser = async (req, res, next) => {
     return next(errorHandler(401, "You can only update your account"));
   }
   try {
+    if (req.body.password) {
+      req.body.password = bcryptjs.hashSync(req.body.password, 10);
+    }
+    const updatedUser = await WaveShaperNode.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
   } catch (error) {
     next(error);
   }
